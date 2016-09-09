9ba53f03c5f2e67fc489a188b771700224b6f69a
Tomas <dostal.it@gmail.com>
2016-09-09 15:37:29 +0100

    testing bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,10 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-bump');
 
     grunt.registerTask('build', ['clean', 'concat:ipushpull', 'uglify:ipushpull']);
+    grunt.registerTask('release', ['build', 'bump']);
 
     grunt.initConfig({
         clean: {
@@ -29,7 +31,15 @@ module.exports = function(grunt){
         uglify: {
             ipushpull: {
                 src: "build/ng-ipushpull.js",
-                dest: "build/ng-ipushpull.min.js"
+                dest: "build/ng-ipushpull.min.js",
+                commitFiles: ["-a"],
+            }
+        },
+
+        bump: {
+            options: {
+                files: ['package.json', 'bower.json'],
+                push: false
             }
         }
     });
