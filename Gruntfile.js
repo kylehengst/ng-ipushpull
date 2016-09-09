c4bfdffac0779eb69fd7fc0106100deb853a1443
Tomas <dostal.it@gmail.com>
2016-09-09 15:02:32 +0100

    Configuring grunt

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt){
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('build', ['clean', 'concat:ipushpull', 'uglify:ipushpull']);
+
+    grunt.initConfig({
+        clean: {
+            build: ['build']
+        },
+
+        concat: {
+            options: {
+                separator: '\r\n'
+            },
+            ipushpull: {
+                dest: 'build/ng-ipushpull.js',
+                src: [
+                    'bower_components/eventEmitter/EventEmitter.min.js',
+                    'bower_components/forge/js/forge.min.js',
+                    'bower_components/socket.io-client/socket.io.js',
+                    'src/ipushpull.ts.js',
+                    'src/**/*.ts.js',
+                ]
+            }
+        },
+
+        uglify: {
+            ipushpull: {
+                src: "build/ng-ipushpull.js",
+                dest: "build/ng-ipushpull.min.js"
+            }
+        }
+    });
+};
